15a8d6b5b370dd9b6f83ef32df740180145c4723
Neerajjangir2001 <neerajjangir2001@gmail.com>
2026-02-10 19:10:43 +0530

    some changes in path env

--- authService/src/main/java/com/authService/config/securityConfig.java
+++ authService/src/main/java/com/authService/config/securityConfig.java
@@ -7,6 +7,7 @@ import com.authService.security.JwtUtil;
 import jakarta.servlet.Filter;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -40,6 +41,9 @@ public class securityConfig {
     private final UserDetailsService userDetailsService;
 
     private final CustomLogoutHandler customLogoutHandler;
+
+    @Value("${fronted.url}")
+    private String frontedUrl;
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -91,7 +95,7 @@ public class securityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration
-                .setAllowedOrigins(List.of("http://localhost:3000", "http://localhost:8080", "http://localhost:5173"));
+                .setAllowedOrigins(List.of(frontedUrl));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
